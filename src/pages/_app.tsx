4c8bd9963f6877282aca82ed8c7434246942ea74
R4R3-M37 <88512151+R4R3-M37@users.noreply.github.com>
2023-01-09 16:42:21 +0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,13 @@ import '@/app/styles/tailwind.css'
 import '@/app/styles/normilize.css'
 import '@/app/styles/global.css'
 
+// @ts-ignore
+const isMobile: boolean = navigator?.userAgentData?.mobile || /iPhone|iPad|iPod|Android/i.test(navigator?.userAgent)
+
+if (isMobile) {
+	window.location.href = 'https://nextjs-kinopoisk.vercel.app/'
+}
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	return (
 		<AppLayout>
