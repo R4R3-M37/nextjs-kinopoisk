99eec768f3a452663fd0c089d3f686796110e9f2
R4R3-M37 <88512151+R4R3-M37@users.noreply.github.com>
2023-01-09 16:44:28 +0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import { Head, Html, Main, NextScript } from 'next/document'
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const isMobile: boolean = navigator?.userAgentData?.mobile || /iPhone|iPad|iPod|Android/i.test(navigator?.userAgent)
-
-if (isMobile) {
-	window.location.href = 'https://cinemata.vercel.app/'
-}
-
 const Document: React.FC = () => {
 	return (
 		<Html lang='ru'>
